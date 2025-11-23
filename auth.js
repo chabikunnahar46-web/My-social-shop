a65f4ed95f17b31e2ba63ca0e7f9ec060d9afd8d
chabikunnahar46-web <chabikunnahar46@gmail.com>
2025-11-23 16:29:57 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,24 +1,21 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { auth, rtdb } from "./Firebase.js";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } 
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyA33of57qBOpIUioKDQ7dMx0KZR6OXLECY",
-    authDomain: "bondhotto-24.firebaseapp.com",
-    projectId: "bondhotto-24",
-    storageBucket: "bondhotto-24.firebasestorage.app",
-    messagingSenderId: "585952718894",
-    appId: "1:585952718894:web:1d9b2e9f12f617a2f2afbf",
-    measurementId: "G-KJQSLPY9NQ"
-  };
+document.getElementById("signupBtn").addEventListener("click", async () => {
+    let email = document.getElementById("email").value;
+    let pass  = document.getElementById("password").value;
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
- 
+    createUserWithEmailAndPassword(auth, email, pass)
+    .then(() => alert("Account Created!"))
+    .catch(e => alert(e.message));
+});
+
+document.getElementById("loginBtn").addEventListener("click", async () => {
+    let email = document.getElementById("email").value;
+    let pass  = document.getElementById("password").value;
+
+    signInWithEmailAndPassword(auth, email, pass)
+    .then(() => alert("Logged In!"))
+    .catch(e => alert(e.message));
+});
