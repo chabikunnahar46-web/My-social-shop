a4aaebfae11a4aaedf6ba1c0adedef61aba4aa85
chabikunnahar46-web <chabikunnahar46@gmail.com>
2025-11-23 09:19:54 +0530

    Update Firebase.js

--- Firebase.js
+++ Firebase.js
@@ -1,27 +1,19 @@
- // Firebase CDN import
-import { initializeApp } 
-    from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
-import { getAuth } 
-    from "https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js";
-
-import { getFirestore } 
-    from "https://www.gstatic.com/firebasejs/10.14.0/firebase-firestore.js";
-
-// Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyA33of57qBOpIUioKDQ7dMx0KZR6OXLECY",
-  authDomain: "bondhotto-24.firebaseapp.com",
-  projectId: "bondhotto-24",
-  storageBucket: "bondhotto-24.firebasestorage.app",
-  messagingSenderId: "585952718894",
-  appId: "1:585952718894:web:1d9b2e9f12f617a2f2afbf",
-  measurementId: "G-KJQSLPY9NQ"
+  apiKey: "YOUR_KEY",
+  authDomain: "YOUR_DOMAIN",
+  databaseURL: "YOUR_DATABASE_URL",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_BUCKET",
+  messagingSenderId: "YOUR_ID",
+  appId: "YOUR_APP_ID"
 };
 
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
-// Initialize Auth + Firestore
 export const auth = getAuth(app);
-export const db = getFirestore(app);
+export const googleProvider = new GoogleAuthProvider();
+export const db = getDatabase(app);
