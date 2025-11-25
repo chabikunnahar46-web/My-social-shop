5cba3201c76b5155ef7de853f953d6fdfe641ec3
chabikunnahar46-web <chabikunnahar46@gmail.com>
2025-11-25 09:53:26 +0530

    Update Firebase.js

--- Firebase.js
+++ Firebase.js
@@ -1,80 +1,24 @@
-// Firebase.js
-
-// Import Firebase Modules
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+// =========================
+// Firebase Config & Setup
+// =========================
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { 
-    getFirestore, 
-    collection, 
-    addDoc, 
-    getDocs 
-} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
+    getDatabase, 
+    ref, 
+    push, 
+    onValue 
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
-import { 
-    getAuth,
-    onAuthStateChanged,
-    signInWithEmailAndPassword,
-    createUserWithEmailAndPassword,
-    signOut
-} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
-
-import {
-    getStorage,
-    ref,
-    uploadBytes,
-    getDownloadURL
-} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-storage.js";
-
-import {
-    getDatabase,
-    ref as dbRef,
-    set,
-    get,
-    child
-} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
-
-
-// ---------- YOUR FIREBASE CONFIG (Correct + Safe) ----------
 const firebaseConfig = {
-  apiKey: "AIzaSyA33of57qBOpIUioKDQ7dMx0KZR6OXLECY",
-  authDomain: "bondhotto-24.firebaseapp.com",
-  databaseURL: "https://bondhotto-24-default-rtdb.firebaseio.com",
-  projectId: "bondhotto-24",
-  storageBucket: "bondhotto-24.firebasestorage.app",
-  messagingSenderId: "585952718894",
-  appId: "1:585952718894:web:1d9b2e9f12f617a2f2afbf",
-  measurementId: "G-KJQSLPY9NQ"
+    apiKey: "AIzaSyA33of57qBOpIUioKDQ7dMx0KZR6OXLECY",
+    authDomain: "bondhotto-24.firebaseapp.com",
+    databaseURL: "https://bondhotto-24-default-rtdb.firebaseio.com",
+    projectId: "bondhotto-24",
+    storageBucket: "bondhotto-24.firebasestorage.app",
+    messagingSenderId: "585952718894",
+    appId: "1:585952718894:web:1d9b2e9f12f617a2f2afbf",
+    measurementId: "G-KJQSLPY9NQ"
 };
-// ------------------------------------------------------------
 
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Setup Services
-const db = getFirestore(app);
-const realtimeDB = getDatabase(app);
-const auth = getAuth(app);
-const storage = getStorage(app);
-
-
-// Export everything for other files
-export {
-    db,
-    auth,
-    storage,
-    realtimeDB,
-    collection,
-    addDoc,
-    getDocs,
-    ref,
-    uploadBytes,
-    getDownloadURL,
-    onAuthStateChanged,
-    signInWithEmailAndPassword,
-    createUserWithEmailAndPassword,
-    signOut,
-    dbRef,
-    set,
-    get,
-    child
-};
+export const db = getDatabase(app);
