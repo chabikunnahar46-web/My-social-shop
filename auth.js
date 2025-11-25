2e477813b0a169722ace0f633bcb8beccbfed96a
chabikunnahar46-web <chabikunnahar46@gmail.com>
2025-11-25 14:03:16 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -5,17 +5,3 @@ from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 document.getElementById("signupBtn").addEventListener("click", async () => {
     let email = document.getElementById("email").value;
     let pass  = document.getElementById("password").value;
-
-    createUserWithEmailAndPassword(auth, email, pass)
-    .then(() => alert("Account Created!"))
-    .catch(e => alert(e.message));
-});
-
-document.getElementById("loginBtn").addEventListener("click", async () => {
-    let email = document.getElementById("email").value;
-    let pass  = document.getElementById("password").value;
-
-    signInWithEmailAndPassword(auth, email, pass)
-    .then(() => alert("Logged In!"))
-    .catch(e => alert(e.message));
-});
