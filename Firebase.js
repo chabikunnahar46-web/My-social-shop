ed8831680f1e30043312c71ec7895647f90ca88f
chabikunnahar46-web <chabikunnahar46@gmail.com>
2025-11-26 09:11:13 +0530

    Update Firebase.js

--- Firebase.js
+++ Firebase.js
@@ -23,3 +23,20 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
 console.log("Firebase Connected:", app);
+// Firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA33of57qBOpIUioKDQ7dMx0KZR6OXLECY",
+  authDomain: "bondhotto-24.firebaseapp.com",
+  databaseURL: "https://bondhotto-24-default-rtdb.firebaseio.com",
+  projectId: "bondhotto-24",
+  storageBucket: "bondhotto-24.firebasestorage.app",
+  messagingSenderId: "585952718894",
+  appId: "1:585952718894:web:1d9b2e9f12f617a2f2afbf"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
+console.log("Firebase Connected", app);
